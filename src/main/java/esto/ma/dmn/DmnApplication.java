7cedfd18f8f1236c62f32f06c8184cff3a911caf
ehaddou <43657214+ehaddou@users.noreply.github.com>
2026-01-02 22:03:26 +0100

    first commit

---
+++ src/main/java/esto/ma/dmn/DmnApplication.java
@@ -0,0 +1,13 @@
+package esto.ma.dmn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DmnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/esto/ma/dmn/DmnApplicationTests.java
@@ -0,0 +1,13 @@
+package esto.ma.dmn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
